95e87d49742740234c11c6662d3faeced044f25c
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2011-11-04 15:50:24 +0100

    Commiting some improvements

--- src/main/java/fr/ifremer/webappsample/Proxy.java
+++ src/main/java/fr/ifremer/webappsample/Proxy.java
@@ -1,23 +1,22 @@
 package fr.ifremer.webappsample;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.URL;
-import java.net.URLConnection;
-import java.nio.CharBuffer;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.jasig.cas.client.authentication.AuthenticationFilter;
-import org.jasig.cas.client.validation.Assertion;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.Namespace;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.XMLOutputter;
+import org.jdom.xpath.XPath;
+
+
+
 
 public class Proxy extends HttpServlet {
 
@@ -26,45 +25,46 @@ public class Proxy extends HttpServlet {
 			throws ServletException, IOException {
 		resp.setContentType("text/plain");
 		
-		//Assertion assertion = (Assertion) req.getSession().getAttribute(AuthenticationFilter.CONST_CAS_ASSERTION);
-		
-		
-//            if(assertion != null && assertion.getPrincipal()!=null) {
-//                String sUser = assertion.getPrincipal().getName();
-//            	resp.getWriter().write("proxy: " + sUser);
-//
-//            } else {
-//            	resp.getWriter().write("proxy: no name");
-//            }
-		
+
 		String ticket = req.getParameter("ticket");
 		System.out.println("[PROXY] proxy ticket received: " + ticket);
 		
 		System.out.println("[PROXY] Trying to validate it against CAS server ...");
 		
-		// It is a pity: this should be handled by the CAS client API ...
+		// following code is a pity: this should be handled by the CAS client API ...
+		// not homebrew piece of code like this
+		
+		// Anyway, it is the way I already implemented it onto GeoNetwork
 		
 		String baseUrl = "https://localhost:8443/cas/proxyValidate";
 		String serviceUrl = "http://localhost:8080/sample-webapp/proxy";
 		
 		StringBuilder response = new StringBuilder();
+		
 		try {
-			InputStream in = new URL(baseUrl + "?service="+serviceUrl+"&ticket="+ ticket).openStream();
-			byte[] cbuf = new byte[1024];
-              int len; // number of bytes read from the stream
-              OutputStream out = new ByteArrayOutputStream();
-              
-              while ((len = in.read(cbuf)) > 0) {
-                  out.write(cbuf, 0, len);
-              }
+			URL casProxyValidateUrl = new URL(baseUrl + "?service="+serviceUrl+"&ticket="+ ticket);
 
-			String proxyResponse = out.toString();
+			Document doc= new SAXBuilder().build(casProxyValidateUrl);
+			String proxyResponse = new XMLOutputter().outputString(doc);
+			
 			System.out.println("[PROXY] got answer from CAS server: " + proxyResponse);
 			
-			response.append("proxy response: ").append(proxyResponse).append("\n");
+			String casUser = "nonamed";
+			
+			Namespace casNs = Namespace.getNamespace("cas", "http://www.yale.edu/tp/cas");
+			
+			XPath xpSuccess = XPath.newInstance("//cas:user");
+			xpSuccess.addNamespace(casNs);
+			casUser = (String) ((Element) xpSuccess.selectSingleNode(doc)).getText();
+
+			System.out.println("[PROXY] parsed user: " + casUser);
+			
+			
+			response.append("proxy acting as user: " + casUser).append("\n");
+			
 		} catch (Exception e) {
-			response.append("proxy response: ").append("failure").append("\n");
 			response.append("exception: ").append(e.getMessage()).append("\n");
+			System.out.println("[PROXY] Exception caught: " + e.getStackTrace());
 		}
 		resp.getWriter().write(response.toString());
 	}
